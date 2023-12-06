51ab180b9439920c8b7eeb30522e09f15d8bad68
riankiurey <146597617+riankiurey@users.noreply.github.com>
2023-12-06 18:54:37 -0300

    Update

--- server2.js
+++ server2.js
@@ -2,6 +2,8 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 const produtos = [
     {
         'id': 0,
@@ -26,3 +28,33 @@ const produtos = [
 
 
 ]
+
+
+app.get('/produtos',(req,res) => {
+    res.status(200).json(produtos)
+
+})
+
+app.post('/produtos',(req,res) => {
+    const id = produtos.length
+    const newProduct = {
+        'id': id,
+        'nome': req.body.nome,
+        'modelo': req.body.modelo
+    }
+    produtos.push(newProduct)
+    res.status(201).json(newProduct)
+})
+
+
+
+
+
+
+
+
+const porta = 3333
+app.listen(porta, function() {
+    console.log(`Rodando na porta ${porta} - ${new Date()}`)
+})
+
